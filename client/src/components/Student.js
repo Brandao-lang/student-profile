6abff9f717fa175eefcf12ed8c8152e1878b6a3f
Brandao-lang <elijahbrandao@gmail.com>
2021-12-14 23:37:40 -0500

    server.js updates

--- client/src/components/Student.js
+++ client/src/components/Student.js
@@ -9,6 +9,7 @@ export default function Student(props) {
 
 
     const getAverage = (grades) => {
+        // this function recieves an array of grades and returns the average by calculating the sum of all grades and divdes by the array length //
         let sum = 0
         
         for (let i = 0; i < grades.length; i++) {
@@ -21,6 +22,7 @@ export default function Student(props) {
     }
 
     const listGrades = (grades) => {
+        // this function maps over an array of grades and returns a list with each grade as a list item //
         return (
             <ul>
                 {grades.map((grade, index) => {
@@ -31,6 +33,7 @@ export default function Student(props) {
     }
 
     const handleChange = (e) => {
+        // this function handles the input for the 'add a tag' text field //
         setInput({
             ...input,
             [e.target.name] : e.target.value
@@ -38,33 +41,53 @@ export default function Student(props) {
     }
 
     const addTag = (e) => {
+        // this function updates both the state array 'tags' with the submitted text from the 'tag-container' form, as well as the parent state sub-array 'tags' within the individual student object //
         e.preventDefault()
 
         let copy = [...tags]
         copy.push(input.tag)
         setTags(copy)
         props.tags.push(input.tag)
+        
+        setInput({
+            ...input,
+            tag: ""
+        })
     }
 
 
     return (
         <div className='student' key={props.index}>
-                <img src={props.pic} alt='profile-pic'/>
-                <button className='grade-toggle-btn' onClick={() => setExpand(!expand)}>{
+            <img src={props.pic} alt='profile-pic'/>
+                
+            <button 
+            className='grade-toggle-btn' 
+            onClick={() => setExpand(!expand)}>
+                {
                     !expand ? '+' : '-'
-                }</button>
+                }
+            </button>
+            
             <h1>{props.firstName} {props.lastName}</h1>
             
             <div className='student-bio'>
                 <p>Email: {props.email}</p>
                 <p>Company: {props.company}</p>
                 <p>Skill: {props.skill}</p>
-                <p>Average: {getAverage(props.grades)}% <button className='mobile-grade-toggle-btn' onClick={() => setExpand(!expand)}>{
-                    !expand ? '+' : '-'
-                }</button></p>
+                <p>Average: {getAverage(props.grades)}% 
+                    <button 
+                    className='mobile-grade-toggle-btn' 
+                    onClick={() => setExpand(!expand)}>
+                        {
+                            !expand ? '+' : '-'
+                        }
+                    </button>
+                </p>
+                
                 <div className={!expand ? 'hide-grades' : 'show-grades'}>
                     {listGrades(props.grades)}
                 </div>
+                
                 <div className='student-tags'>
                 {
                     props.tags.map((tag, index) => {
@@ -74,12 +97,14 @@ export default function Student(props) {
                     })
                 }
                 </div>
+                
                 <div className='tag-container'>
                     <form onSubmit={addTag}>
                         <input 
                             type='text'
-                            placeholder='Add a Tag'
+                            placeholder='Add a tag'
                             name='tag'
+                            value={input.tag}
                             onChange={handleChange}
                         />
                     </form>
--- server.js
+++ server.js
@@ -2,17 +2,14 @@ const express = require('express')
 
 const app = express()
 app.use(express.json())
-const PORT = 3000
+const PORT = process.env.PORT || 3000
+
+app.use(express.static(path.join(__dirname, 'client/build')))
 
 
-
-
-
-
-
-
-
-
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname+'/client/build/index.html'));
+  });
 
 
 app.listen(PORT, () => {
